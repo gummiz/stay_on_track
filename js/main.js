b3f423f7099e5a0be64ef85a42a512d0c38d4571
Tobias Conrad <max.tobias.conrad@gmail.com>
2022-06-28 09:04:55 +0200

    implemented win condition

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const startBtnEl = document.getElementById("start-btn")
 // create Board
 const gridBoard = document.createElement("div")
+const gameEl = document.getElementById("game")
         gridBoard.id = "board"
+        console.log(gameEl)
         document.body.appendChild(gridBoard)
 
 
@@ -17,35 +19,42 @@ class Game {
         this.player.createPlayer()
         this.createBoard()
     }
-    start (){
-        this.createPathX1()
-    }
+    
     movePlayer(){
         document.addEventListener("keydown", (event) => {
             if(event.key === "ArrowLeft") {
                 this.player.moveLeft()
                 if( !this.tiles[this.player.playerPos[0]][this.player.playerPos[1]].className.includes("tomato") ) 
                 console.log("you lose")
-                   
+                else if (this.player.playerPos[0] === 6 && this.player.playerPos[1] === 9) 
+                console.log("You Win!")
             }
             else if (event.key === "ArrowRight") {
                 this.player.moveRight()
                 if( !this.tiles[this.player.playerPos[0]][this.player.playerPos[1]].className.includes("tomato") ) 
                 console.log("you lose")
+                else if (this.player.playerPos[0] === 6 && this.player.playerPos[1] === 9) 
+                console.log("You Win!")
                 }
             else if (event.key === "ArrowDown") {
                 this.player.moveDown()
                 if( !this.tiles[this.player.playerPos[0]][this.player.playerPos[1]].className.includes("tomato") ) 
                 console.log("you lose")
+                else if (this.player.playerPos[0] === 6 && this.player.playerPos[1] === 9) 
+                console.log("You Win!")
             }
             else if ( event.key === "ArrowUp") {
                 this.player.moveUp()
                 if( !this.tiles[this.player.playerPos[0]][this.player.playerPos[1]].className.includes("tomato") ) 
                 console.log("you lose")
+                else if (this.player.playerPos[0] === 6 && this.player.playerPos[1] === 9) 
+            console.log("You Win!")
             }
+            
+            
         })
     }
-    checkForCollision(){}   
+  
     createBoard(){
         for (let i = 0; i < 8; i++){
             this.tiles[i]=[] 
@@ -53,14 +62,30 @@ class Game {
                 {
                     const newTile = document.createElement("div")
                     newTile.className = "newTiles" 
-                
                     gridBoard.appendChild(newTile)
                     this.tiles[i].push(newTile)
             }
         }
     }
-    createPathX1(){
-        
+    start(){
+    startBtnEl.addEventListener("click", ()=>{
+        let counter =6;
+        const intId= setInterval(callback, 1000)
+        function callback(){
+            if (counter > 0){
+                counter--
+            }
+            else {
+                clearInterval(intId)
+                console.log("Good Luck")
+                const pathTiles= document.getElementsByClassName("tomato")
+                for (let i = 0; i < pathTiles.length; i++){
+                    pathTiles[i].style.backgroundColor ="white"
+                }
+            }
+            console.log(counter)
+        }
+
         this.tiles[0][0].classList.add("tomato")
         this.tiles[0][1].classList.add("tomato")
         this.tiles[1][1].classList.add("tomato")
@@ -77,6 +102,7 @@ class Game {
         this.tiles[5][8].classList.add("tomato")
         this.tiles[6][8].classList.add("tomato")
         this.tiles[6][9].classList.add("tomato")
+       })
     }
 }
 
@@ -88,6 +114,7 @@ class Player {
         this.positionX = 16;
         this.positionY = 16;
         this.player = this.createPlayer()
+         
        
     }
     moveUp(){
@@ -137,8 +164,5 @@ const game = new Game()
 
 game.onLoad()
 game.start()
-
-
-
 
 
